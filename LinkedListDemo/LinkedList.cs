90d629c2917077f9b6bbbe4b377ff1d45c646a6d
aayusharya1 <aayusharya07@gmail.com>
2020-10-27 00:23:50 +0530

    [Aayush] Add. Code for adding elements to linkedlist in LIFO Format.

---
+++ LinkedListDemo/LinkedList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedListDemo
+{
+    class LinkedList
+    {
+        public Node head;
+
+        internal void AddToStack(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+                node.next = null;
+            else
+                node.next = this.head;
+            this.head = node;
+            Console.WriteLine("{0} is inserted at the head ", node.data);
+        }
+
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked List is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + "\t");
+                temp = temp.next;
+            }
+            Console.WriteLine("\n ______________________________________________________________________");
+        }
+    }
+    
+}
---
+++ LinkedListDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LinkedListDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            LinkedList stack = new LinkedList();
+            stack.AddToStack(70);
+            stack.AddToStack(30);
+            stack.AddToStack(56);
+            stack.Display();
+
+        }
+    }
+}
