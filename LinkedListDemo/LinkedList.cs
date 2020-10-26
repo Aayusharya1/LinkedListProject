9eafeff31b8b3b8a845cb4d9386301265d2f59d1
aayusharya1 <aayusharya07@gmail.com>
2020-10-27 01:23:49 +0530

    [Aayush] Add. Code for adding element at the back of linked list.

--- LinkedListDemo/LinkedList.cs
+++ LinkedListDemo/LinkedList.cs
@@ -19,6 +19,23 @@ namespace LinkedListDemo
             Console.WriteLine("{0} is inserted at the head ", node.data);
         }
 
+        public void AddToBack(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} is inserted into the linked list", node.data);
+        }
+
         public void Display()
         {
             Node temp = this.head;
--- LinkedListDemo/Program.cs
+++ LinkedListDemo/Program.cs
@@ -8,9 +8,9 @@ namespace LinkedListDemo
         {
             Console.WriteLine("Hello World!");
             LinkedList stack = new LinkedList();
-            stack.AddToStack(70);
-            stack.AddToStack(30);
-            stack.AddToStack(56);
+            stack.AddToBack(56);
+            stack.AddToBack(30);
+            stack.AddToBack(70);
             stack.Display();
 
         }
