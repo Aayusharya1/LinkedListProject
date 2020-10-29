3aacf765c4b69dd583e9311090edbddf3d356eb5
aayusharya1 <aayusharya07@gmail.com>
2020-10-30 01:45:28 +0530

    [Aayush] Add. Method for inserting element in the list

--- LinkedListDemo/Program.cs
+++ LinkedListDemo/Program.cs
@@ -7,11 +7,17 @@ namespace LinkedListDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            LinkedList stack = new LinkedList();
-            stack.AddToBack(56);
-            stack.AddToBack(30);
-            stack.AddToBack(70);
-            stack.Display();
+            LinkedList list = new LinkedList();
+            //stack.AddToBack(56);
+            //stack.AddToBack(30);
+            //stack.AddToBack(70);
+            //stack.Display();
+
+            //Inserting number at specific position
+            list.AddToBack(56);
+            list.AddToBack(70);
+            list.InsertAtParticularPosition(2, 30);
+            list.Display();
 
         }
     }
